d93274c5bbc3d841aad4bd83c1601288efc349f5
SulavkoPavel <SulavkoPavel@users.noreply.github.com>
2021-01-07 18:10:09 +0300

    Initial commit

---
+++ src/main/java/ru/sulavko/springcourse/dao/PersonMapper.java
@@ -0,0 +1,24 @@
+package ru.sulavko.springcourse.dao;
+
+import org.springframework.jdbc.core.RowMapper;
+import ru.sulavko.springcourse.models.Person;
+
+import javax.swing.tree.TreePath;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonMapper implements RowMapper<Person>{
+
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setEmail(resultSet.getString("email"));
+        person.setAge(resultSet.getInt("age"));
+
+        return person;
+    }
+}
+
